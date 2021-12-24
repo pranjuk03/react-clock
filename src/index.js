6d9ff9d73f68872c8ecd50e94fd58f17ef50f614
pranjuk03 <pranjukarjawkar@gmail.com>
2021-12-24 14:56:26 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import'bootstrap/dist/css/bootstrap.min.css';
+import $ from'jquery';
+import'bootstrap/dist/js/bootstrap.bundle.min';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppComponent from './components/App';
+import './styles/app.css';
+
+ReactDOM.render(<AppComponent/>,document.getElementById('root'));
